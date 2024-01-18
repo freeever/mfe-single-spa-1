f493ac32339f09eb4cd95ac7334ecb4da280b811
Daniel Xu <daniel.xu4@ontario.ca>
2024-01-18 09:46:28 -0500

    Update for Single-SPA

--- carts-app/src/app/app-routing.module.ts
+++ carts-app/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
+import { EmptyRouteComponent } from './../../../mfe1/src/app/empty-route/empty-route.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '**', component: EmptyRouteComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
