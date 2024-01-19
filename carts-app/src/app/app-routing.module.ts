02ecee4d6b3c3bc0d10b27c68da9c57c2ca07d86
Daniel Xu <daniel.xu4@ontario.ca>
2024-01-19 13:48:02 -0500

    Troubleshooting: Routing in the carts-app. Solution: Added APP_BASE_HREF provider

--- carts-app/src/app/app-routing.module.ts
+++ carts-app/src/app/app-routing.module.ts
@@ -1,14 +1,18 @@
 import { EmptyRouteComponent } from './../../../mfe1/src/app/empty-route/empty-route.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { APP_BASE_HREF } from '@angular/common';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 const routes: Routes = [
+  { path: '', component: DashboardComponent },
   { path: 'carts', loadChildren: () => import('./carts/carts.module').then(m => m.CartsModule) },
   { path: '**', component: EmptyRouteComponent }
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
+  providers: [{ provide: APP_BASE_HREF, useValue: '/carts-app' }],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
